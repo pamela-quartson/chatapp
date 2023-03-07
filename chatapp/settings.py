99135d446e5d0b4ef244609fad263d4bb056a0d2
Pamela Quartson <pamquartz31@gmail.com>
2023-03-07 22:21:36 +0000

    Postgres db configured

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -81,8 +81,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': env('DB_HOST'),
+        'USER': env('DB_USER'),
+        'NAME': env('DB_NAME'),
+        'PORT': env('DB_PORT'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
